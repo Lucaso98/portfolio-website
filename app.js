89ca7a94aa8dfca4de864d5cff7163fbedd4786a
Lucas <lucas.fransson98@gmail.com>
2025-08-11 20:53:10 +0200

    Added javascript for links and cleaned up code

---
+++ app.js
@@ -0,0 +1,11 @@
+var navbar = document.getElementById("navbar");
+var navLinks = navbar.getElementsByClassName("nav-link");
+
+for (let i = 0; i < navLinks.length; i++) {
+   navLinks[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+   });
+
+}
